f362c1e5901825a707970e1d701e42cc13a96dad
sonyway <baotoantrinh2002@gmail.com>
2022-11-08 22:15:30 +0700

    Add MVC

---
+++ Winform/Lab78910/MVC/Controllers/OrderDetailsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        private DevConn db = new DevConn();
+
+        // GET: OrderDetails
+        public ActionResult Index()
+        {
+            return View(db.OrderDetails.ToList());
+        }
+
+        // GET: OrderDetails/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            if (orderDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderDetail);
+        }
+
+        // GET: OrderDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,OrderID,ItemID,Quantity,UnitAmount")] OrderDetail orderDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.OrderDetails.Add(orderDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(orderDetail);
+        }
+
+        // GET: OrderDetails/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            if (orderDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderDetail);
+        }
+
+        // POST: OrderDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,OrderID,ItemID,Quantity,UnitAmount")] OrderDetail orderDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(orderDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(orderDetail);
+        }
+
+        // GET: OrderDetails/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            if (orderDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(orderDetail);
+        }
+
+        // POST: OrderDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            OrderDetail orderDetail = db.OrderDetails.Find(id);
+            db.OrderDetails.Remove(orderDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
